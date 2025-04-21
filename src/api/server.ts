85e6f1032a2923878e3957cb48a4ac66138f1b6d
Sorbon <mark.jonsan@bk.ru>
2025-04-21 23:20:38 +0500

    Test server.ts 5

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import cors from "cors";
 
 // Создаем Express-приложение
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-// Middleware
 app.use(
   cors({
     origin: ["https://fin-manager-eta.vercel.app", "http://localhost:3000"],
@@ -18,24 +18,41 @@ app.get("/api/health", (req: Request, res: Response) => {
   res.status(200).json({ status: "OK" });
 });
 
-app.get("/api/exchange-rates", async (req: Request, res: Response) => {
+// app.get("/api/exchange-rates", async (req: Request, res: Response) => {
+//   try {
+//     const response = await fetch("https://valuta.tj/parser/echokurs.php");
+//     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+//     const data = await response.json();
+//     const ratesArray = Object.values(data);
+
+//     res.setHeader("Cache-Control", "s-maxage=3600").json(ratesArray);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+export default async (req: VercelRequest, res: VercelResponse) => {
   try {
-    const response = await fetch("https://valuta.tj/parser/echokurs.php");
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const apiResponse = await fetch("https://valuta.tj/parser/echokurs.php");
 
-    const data = await response.json();
-    const ratesArray = Object.values(data);
+    if (!apiResponse.ok) {
+      return res
+        .status(apiResponse.status)
+        .json({ error: "Upstream API error" });
+    }
 
-    res.setHeader("Cache-Control", "s-maxage=3600").json(ratesArray);
+    const data = await apiResponse.json();
+
+    return res
+      .setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate")
+      .status(200)
+      .json(Object.values(data));
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error("Error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-});
-
-// Решение для Vercel
-const vercelHandler = (req: Request, res: Response) => app(req, res);
-export default vercelHandler;
+};
 
 // Локальный сервер
 if (process.env.NODE_ENV !== "production") {
